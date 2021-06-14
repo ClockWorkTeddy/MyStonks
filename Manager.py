a58794921875f6ccdd7868a0d6a15849aba346ca
Ivan <Vanka03@ya.ru>
2021-06-14 22:30:20 +0300

    New features
    Added writing to GSheets
    Added saving sites

--- Manager.py
+++ Manager.py
@@ -1,21 +1,49 @@
 import pygsheets
 import numpy as np
+import requests
+
+def CellName(column, row_num):
+    name = column + str(row_num)
+    return name
+
+def GetCell(column, row_num):
+    adress = CellName(column, row_num)
+    value = wks.cell(adress).value
+    return value
+
+def SetCell(column, row_num, value):
+    adress = CellName(column, row_num)
+    wks.cell(adress).value = value
+
+def GetTicker(row_num):
+    ticker = GetCell('B', row_num)
+    return ticker
+
+def GetSite(ticker):
+    url = "https://finviz.com/quote.ashx?t="+ticker
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'}
+    r = requests.get(url, headers = headers)
+    print(r.encoding)
+    with open('test.html', 'w', encoding="utf-8") as output_file:
+        output_file.write(r.text)
 
 gc = pygsheets.authorize()
 
-# Open spreadsheet and then worksheet
-#res = gc.sheet.create("sampleTitle")
+# Open spreadsheet
 sh = gc.open('sampleTitle')
 wks = sh.sheet1
+start_row_index = 2
+# Read Tickers from cells in 1sr column do-while
+row_index = start_row_index
 
-# Update a cell with value (just to let him know values is updated ;) )
-wks.update_value('A1', "Hey yank this numpy array")
-my_nparray = np.random.randint(10, size=(3, 4))
-
-# update the sheet with array
-wks.update_values('A2', my_nparray.tolist())
-
-# share the sheet with your friend
-sh.share("zhukovent@gmail.com")
+while (True):
+    ticker = GetTicker(row_index)
+    if ticker != '':
+        GetSite(ticker)
+        #SetCell('E', row_index, "=GOOGLEFINANCE(\""+ticker+"\",\"pe\")")
+        row_index += 1
+    else:
+        break
 
 print("hui")
+
