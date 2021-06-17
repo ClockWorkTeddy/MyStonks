8c835df38b6963b2f60a2ffa3c8c422115e4004e
Ivan <Vanka03@ya.ru>
2021-06-17 07:03:42 +0300

    Parcing

--- Manager.py
+++ Manager.py
@@ -1,6 +1,7 @@
 import pygsheets
 import numpy as np
 import requests
+from bs4 import BeautifulSoup
 
 def CellName(column, row_num):
     name = column + str(row_num)
@@ -19,13 +20,19 @@ def GetTicker(row_num):
     ticker = GetCell('B', row_num)
     return ticker
 
-def GetSite(ticker):
-    url = "https://finviz.com/quote.ashx?t="+ticker
+def GetSiteText(ticker, url):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'}
-    r = requests.get(url, headers = headers)
+    r = requests.get(url + ticker, headers = headers)
     print(r.encoding)
-    with open('test.html', 'w', encoding="utf-8") as output_file:
-        output_file.write(r.text)
+    return r.text
+    #with open('test.html', 'w', encoding="utf-8") as output_file:
+        #output_file.write(r.text)
+
+def ParseFinViz(text):
+    soup = BeautifulSoup(text)
+    peg = soup.find_all('div', {'class': ['item', 'item even']})
+    i = peg.find('td', {'class': 'snapshot-td2'}).find('b').text
+    p = 1
 
 gc = pygsheets.authorize()
 
@@ -35,11 +42,13 @@ wks = sh.sheet1
 start_row_index = 2
 # Read Tickers from cells in 1sr column do-while
 row_index = start_row_index
+finviz_url = "https://finviz.com/quote.ashx?t="
 
 while (True):
     ticker = GetTicker(row_index)
     if ticker != '':
-        GetSite(ticker)
+        fin_viz_text = GetSiteText(ticker, finviz_url)
+        ParseFinViz(fin_viz_text)
         #SetCell('E', row_index, "=GOOGLEFINANCE(\""+ticker+"\",\"pe\")")
         row_index += 1
     else:
