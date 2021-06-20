64fc89164ff1794b1867f9dc87594a21ab953211
Ivan <Vanka03@ya.ru>
2021-06-20 22:13:42 +0300

    Done, it might needs refactoring

--- Manager.py
+++ Manager.py
@@ -1,7 +1,9 @@
 import pygsheets
 import numpy as np
 import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup
+import time
 
 
 def CellName(column, row_num):
@@ -24,45 +26,62 @@ def GetTicker(row_num):
 def GetSite(page_url, ticker):
     url = page_url + ticker
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'}
-    r = requests.get(url, headers = headers)
-    with open('test.html', 'w', encoding="utf-8") as output_file:
-        output_file.write(r.text)
-
+    r = requests.get(url, timeout=30, headers = headers)
     return r.text
 
 def GetFinViz(ticker):
-    url = "https://finviz.com/quote.ashx?t="
-    html_text = GetSite(url, ticker)
     str = []
-    st = ''
+    html_text = GetSite("https://finviz.com/quote.ashx?t=", ticker)
     soup = BeautifulSoup(html_text, 'html.parser')
-    
     table = soup.find_all('td', class_ = 'snapshot-td2')
-    #with open('test.html', 'w', encoding="utf-8") as output_file:
-        #output_file.write(st)
-
     str.append(table[13].text)
     str.append(table[38].text)
-    print(str)
+    return str
 
+def GetHtml(wd):
+    time_out = 0.5
+    wd.get("https://finance.yahoo.com/quote/" + ticker)
+    time.sleep(time_out)
+    wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(time_out)
+    wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    html_text = wd.execute_script('return document.body.innerHTML;')
+    return html_text
+
+
+def GetYahoo(ticker, wd):
+    res = []
+    html_text = GetHtml(wd)
+    soup = BeautifulSoup(html_text)
+    coeffs = soup.find_all('div', class_ = 'Bdbw(2px) Bdbs(s) Bdbc($seperatorColor) H(1em) Pos(r) Mt(30px) Mx(10%)')
+    coeff = coeffs[0].contents[5].contents[0]
+    res.append(coeff)
+    price = soup.find_all('div', class_ = 'Pos(r) T(5px) Miw(100px) Fz(s) Fw(500) D(ib) C($primaryColor)Ta(c) Translate3d($half3dTranslate)')
+    data = price[0].contents[4].contents[0]
+    res.append(data)
+    return res
+   
 
 gc = pygsheets.authorize()
-
-# Open spreadsheet
 sh = gc.open('sampleTitle')
 wks = sh.sheet1
-start_row_index = 2
-# Read Tickers from cells in 1sr column do-while
-row_index = start_row_index
+row_index = 2
+wd = webdriver.Chrome()
 
 while (True):
     ticker = GetTicker(row_index)
     if ticker != '':
-        GetFinViz(ticker)
-        #SetCell('E', row_index, "=GOOGLEFINANCE(\""+ticker+"\",\"pe\")")
+        fin_viz_data = GetFinViz(ticker)
+        SetCell('F', row_index, fin_viz_data[0])
+        SetCell('G', row_index, fin_viz_data[1])
+        yahoo_data = GetYahoo(ticker, wd)
+        SetCell('K', row_index, yahoo_data[0])
+        SetCell('J', row_index, yahoo_data[1])
         row_index += 1
     else:
         break
+
+wd.close()
 
 print("hui")
 
