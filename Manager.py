07312db6c6d54621e61a22ff6693e5fc4f027455
Ivan <Vanka03@ya.ru>
2022-09-11 21:50:21 +0300

    Refacted

--- Manager.py
+++ Manager.py
@@ -20,24 +20,22 @@ def SetCell(column, row_num, value, wks):
 #+
 def GetTicker(row_num, wks):
     ticker = GetCell('B', row_num, wks)
+    
     return ticker
 #+
 def GetYahooApi(ticker):
     res = []
     company = yf.Ticker(ticker)
-    rec = company.info['recommendationMean']
-    price = company.info['targetMeanPrice']
-    res.append(rec)
-    res.append(price)
+    res.append(company.info['recommendationMean'])
+    res.append(company.info['targetMeanPrice'])
+    
     return res
 #+
 def GetFinvizApi(ticker):
     res = []
     data = fv.get_stock(ticker)
-    peg = data['PEG']
-    eps = data['EPS past 5Y']
-    res.append(peg)
-    res.append(eps)    
+    res.append(data['PEG'])
+    res.append(data['EPS past 5Y'])    
     
     return res
 
@@ -55,10 +53,30 @@ def OverLimit(row_index, rows_qnt, limit):
         else:
             return TRUE
 
-def Main():
+def GetWorkSheet() :
     gc = pygsheets.authorize()
     sh = gc.open('Daily')
-    wks = sh.worksheet('title', 'Stonks')
+    return sh.worksheet('title', 'Stonks')
+
+def FinVizProcessing(row_index, wks, ticker) :
+    fin_viz_data = GetFinvizApi(ticker)
+    SetCell('G', row_index, fin_viz_data[0], wks)
+    SetCell('H', row_index, fin_viz_data[1], wks)
+
+def YahooFinProcessing(row_index, wks, ticker) :
+    yahoo_data = GetYahooApi(ticker)
+    SetCell('L', row_index, yahoo_data[0], wks)
+    SetCell('K', row_index, yahoo_data[1], wks)
+
+def FillCells(row_index, date_time, wks, ticker) :
+    SetCell('C', row_index, date_time, wks)
+    FinVizProcessing(row_index, wks, ticker)
+    YahooFinProcessing(row_index, wks, ticker)
+    
+    print(ticker + str(row_index))
+
+def Main():
+    wks = GetWorkSheet()
     row_index = int(input()) + 1
     rows_qnt = int(input())
     limit = row_index + rows_qnt
@@ -66,15 +84,9 @@ def Main():
     while (True):
         ticker = GetTicker(row_index, wks)
         date_time = GetTimeStamp()
+
         if ticker != '' and OverLimit(row_index, rows_qnt, limit) != FALSE:
-            SetCell('C', row_index, date_time, wks)
-            fin_viz_data = GetFinvizApi(ticker)
-            SetCell('G', row_index, fin_viz_data[0], wks)
-            SetCell('H', row_index, fin_viz_data[1], wks)
-            yahoo_data = GetYahooApi(ticker)
-            SetCell('L', row_index, yahoo_data[0], wks)
-            SetCell('K', row_index, yahoo_data[1], wks)
-            print(ticker + str(row_index))
+            FillCells(row_index, date_time, wks, ticker)
             row_index += 1
         else:
             break
